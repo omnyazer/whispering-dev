3308739e847d72f9e14376c027a2da5c6ef8eb92
syaob <syaob@yahoo.fr>
2024-12-06 11:38:56 +0100

    Correction de .gitignore pour ignorer node_modules

--- README.md
+++
@@ -1,1 +0,0 @@
-# react-ejs-app-whispers
--- index.js
+++
@@ -1,5 +0,0 @@
-import { app } from "./server.js";
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Running in http://localhost:${port}`);
-});
