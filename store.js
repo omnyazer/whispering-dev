d13fc814c04c6da869cf3f08dee54aca6c1a9208
omnya zerghouni <omnyazer@gmail.com>
2025-06-20 14:31:35 +0200

    Format JSON lisible dans db.json

--- store.js
+++ store.js
@@ -2,7 +2,7 @@ import fs from "node:fs/promises";
 import path from "node:path";
 
 const filename = path.join(process.cwd(), "db.json");
-const saveChanges = (data) => fs.writeFile(filename, JSON.stringify(data));
+const saveChanges = (data) => fs.writeFile(filename, JSON.stringify(data, null, 2));
 const readData = async () => {
   const data = await fs.readFile(filename, "utf-8");
   return JSON.parse(data);
